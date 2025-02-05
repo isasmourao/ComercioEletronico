8b04cb7f4273d25dfc8d09048353ed874b880941
Isabella Dias <isabellamouraodias@gmail.com>
2025-02-04 23:14:55 -0300

    commit teste

---
+++ src/main/java/br/ufjf/dcc/dcc025/comercioeletronico/ComercioEletronico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ufjf.dcc.dcc025.comercioeletronico;
+
+/**
+ *
+ * @author isabe
+ */
+public class ComercioEletronico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
